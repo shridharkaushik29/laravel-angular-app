005dcc9cdf53745cf2c712d6e6706c807b624059
Your Name <shridharkaushik29@gmail.com>
2018-04-23 13:45:55 +0530

    Added files

---
+++ Angular/Facades/App.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Shridhar\Angular\Facades;
+
+/**
+ * Description of App
+ *
+ * @author ubuntu
+ */
+class App {
+
+    static function get() {
+        return "App";
+    }
+
+}
