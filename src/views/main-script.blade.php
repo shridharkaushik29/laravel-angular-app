2ede9fffe9f120719af64e32b103e9ad224ae7b9
Shridhar Sharma <shridharkaushik29@gmail.com>
2019-09-01 14:53:36 +0530

    Updated the main-script to use Object.assign

--- src/views/main-script.blade.php
+++ src/views/main-script.blade.php
@@ -1,19 +1,3 @@
 <script>
-    <?php
-    /** @var Shridhar\Angular\App $app */
-    $app->js_vars()->each(function ($value, $name) {
-        if (is_object($value) || is_array($value)) {
-            $value = json_encode($value);
-        } elseif (is_string($value)) {
-            $value = "\"$value\"";
-        } elseif ($value === null) {
-            $value = "null";
-        } elseif ($value === false) {
-            $value = "false";
-        } elseif ($value === true) {
-            $value = "true";
-        }
-        echo "var $name = $value;";
-    });
-    ?>
+    <?php echo "Object.assign(window, " . json_encode($app->js_vars()) . ")";?>
 </script>
