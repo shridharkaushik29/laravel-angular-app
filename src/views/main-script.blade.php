59a028659b6100d16869aef1ad4efeaca106ac13
Shridhar Sharma <shridharkaushik29@gmail.com>
2019-09-01 12:51:10 +0530

    bug fixed

--- src/views/main-script.blade.php
+++ src/views/main-script.blade.php
@@ -2,10 +2,10 @@
     <?php
     /** @var Shridhar\Angular\App $app */
     $app->js_vars()->each(function ($value, $name) {
-        if (!is_bool($value) && !is_integer($value)) {
-            $value = "\"$value\"";
-        } elseif (is_iterable($value)) {
+        if (is_object($value) || is_array($value)) {
             $value = json_encode($value);
+        } elseif (!is_bool($value) && !is_integer($value)) {
+            $value = "\"$value\"";
         }
         echo "var $name = $value;";
     });
