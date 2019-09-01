d3e73b13e774c24444298d8ffad474b81b6bcebc
Shridhar Sharma <shridharkaushik29@gmail.com>
2019-09-01 12:08:56 +0530

    Upgraded to Laravel 5.8 and added support for iteratable js variable

--- src/views/main-script.blade.php
+++ src/views/main-script.blade.php
@@ -1,8 +1,11 @@
 <script>
     <?php
+    /** @var Shridhar\Angular\App $app */
     $app->js_vars()->each(function ($value, $name) {
         if (!is_bool($value) && !is_integer($value)) {
             $value = "\"$value\"";
+        } elseif (is_iterable($value)) {
+            $value = json_encode($value);
         }
         echo "var $name = $value;";
     });
